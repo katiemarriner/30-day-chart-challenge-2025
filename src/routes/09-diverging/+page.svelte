157bbd0784b57381774c9e0b02a862599945d04e
Katie Marriner <kemarriner@gmail.coml>
2025-04-10 11:50:17 -0400

    test data as static asset

--- src/routes/09-diverging/+page.svelte
+++ src/routes/09-diverging/+page.svelte
@@ -1,1 +1,15 @@
-Day 9: Diverging
+<script>
+  import { onMount } from "svelte";
+  import { scaleLinear, scaleOrdinal } from 'd3-scale';
+
+  let data;
+  onMount(async () => {
+    const res = await fetch('09-diverging.json');
+    data = await res.json();
+  });
+  
+  $: console.log(data)
+</script>
+
+<!-- for each country create a div that will hold an svg pyramid chart -->
+<!-- base line is half of the width, vertical axis is ordinal, horizontal is linear -->
